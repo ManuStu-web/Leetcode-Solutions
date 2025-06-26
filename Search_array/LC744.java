727b00e51044d11cb278f2f35cc683d23fcc6c27
Manu Sharma <Msharma.stu@gmail.com>
2025-06-26 18:45:39 +0530

    Leetcode Problem 744

---
+++ Search_array/LC744.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class LC744
+{
+    static char searching(char ch[], char target)
+    {
+      int lb=0;
+      int ub=ch.length-1;
+      char ans=' ';
+
+      if(target>=ch[ub])
+      {
+        return ch[0];
+      }
+
+      while(lb<=ub)
+      {
+        int mid=lb+(ub-lb)/2;
+        if(ch[mid]<=target)
+        {
+            lb=mid+1;
+        }
+        else
+        {
+            ub=mid-1;
+        }
+        if(ch[mid]<target && ch[mid+1]>target)
+        {
+            ans=ch[mid+1];
+        }
+      }
+
+      return ans;
+    }
+
+
+    public static void main(String args)
+    {
+        Scanner in = new Scanner(System.in);
+
+        char ch[]={'c','f','j'};
+        char target='a';
+
+        searching(ch,target);
+    }
+}
