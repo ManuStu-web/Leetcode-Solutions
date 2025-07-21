7d88262a2bd8fa535b4b725fa78087735e8aa928
Manu Sharma <Msharma.stu@gmail.com>
2025-07-21 21:46:29 +0530

    Leetcode Problem 101

---
+++ BinaryTrees/Symmetric.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) 
+    {
+        if(root==null)
+        {
+            return true;
+        }
+
+        Queue<TreeNode> store = new LinkedList<>();
+        store.offer(root.left);
+        store.offer(root.right);
+         while(!store.isEmpty())
+        {
+            TreeNode left = store.poll();
+            TreeNode right = store.poll();
+
+            if(left==null && right == null)
+            {
+               continue;
+            }
+
+            if(left==null|| right==null)
+            {
+                return false;
+            }
+            
+            if(left.val!=right.val)
+            {
+                return false;
+            }
+
+            store.offer(left.left);
+            store.offer(right.right);
+            store.offer(left.right);
+            store.offer(right.left);
+        }
+
+        return true;
+
+    }
+}
