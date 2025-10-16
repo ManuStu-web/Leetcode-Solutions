a9caef876c69ca31f0bba39e9520bdbbe15a51d4
Manu Sharma <Msharma.stu@gmail.com>
2025-10-16 20:52:29 +0530

    Leetcode Problem 230

---
+++ Trees/KthSmallest.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int count=0;
+    int result=-1;
+    public int kthSmallest(TreeNode root, int k) 
+    {
+        helper(root,k);
+        return result;
+    }
+
+    public void helper(TreeNode node ,int k)
+    {
+        if(node==null)
+        {
+            return;
+        }
+
+       helper(node.left,k);
+        count+=1;
+
+        if(count==k)
+        {
+            result=node.val;
+            return;
+        }
+
+        helper(node.right,k);
+    }
+}
