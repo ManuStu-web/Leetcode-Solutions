04b3090412550ade05686197f0a5e777b4e93bf8
Manu Sharma <Msharma.stu@gmail.com>
2025-10-29 19:57:47 +0530

    Leetcode Problem 1248

---
+++ Arrays/NumOfNiceSubarray.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int numberOfSubarrays(int[] nums, int k) 
+    {
+        return helper(nums,k)-helper(nums,k-1);
+    }
+
+    public int helper(int nums[],int k)
+    {
+         int l=0,r=0,sum=0,count=0;
+
+        while(r<nums.length)
+        {
+            sum+=nums[r]%2;
+            while(sum>k)
+            {
+                sum-=nums[l]%2;
+                l++;
+            }
+            count+=(r-l+1);
+            r++;
+        }
+
+        return count;
+    }
+}
