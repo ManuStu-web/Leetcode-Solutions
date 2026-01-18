edbb9d49e11286e8980c423034eba704f13d1227
Manu Sharma <Msharma.stu@gmail.com>
2026-01-18 11:09:58 +0530

    Leetcode Problem 50

---
+++ Arrays/Power.java
@@ -0,0 +1,31 @@
+class Solution {
+    public double myPow(double x, int n) 
+    {
+        if(x==0) return 0;
+        if(n==0) return 1;
+        if(x==1) return 1;
+        if(x==-1 && n%2 ==0) return 1;
+        if(x==-1 && n%2 !=0) return -1;
+
+        long binaryForm = n;
+        if(n<0)
+        {
+            x=(1/x);
+            binaryForm=-binaryForm;
+        }
+        double ans=1;
+
+        while(binaryForm>0)
+        {
+            if(binaryForm % 2 == 1)
+            {
+                ans *= x;
+            }
+
+             x= x*x;
+            binaryForm/=2;
+        }
+
+        return ans;
+    }
+}
