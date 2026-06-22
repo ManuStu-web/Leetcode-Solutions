785aab46cab96e0fac3c6e005fea2feb460462ac
Manu Sharma <Msharma.stu@gmail.com>
2026-06-22 21:16:01 +0530

    Leetcode Problem 455

---
+++ Greedy/AssignCookies.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int l=0;
+        int r=0;
+        while(l<g.length && r<s.length)
+        {
+            if(r==s.length)
+            {
+                break;
+            }
+            
+            if(s[r]>=g[l])
+            {
+                l++;
+                r++;
+            }
+            else
+            {
+                r++;
+            }
+        }
+
+        return l;
+    }
+}
