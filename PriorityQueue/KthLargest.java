5b5371769b9c7701dede30839cce0601952a0128
Manu Sharma <Msharma.stu@gmail.com>
2026-07-11 18:50:01 +0530

    Leetcode Problem 215

---
+++ PriorityQueue/KthLargest.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for(int num : nums)
+        {
+            if(minHeap.size()<k)
+            {
+                minHeap.offer(num);
+            }
+            else if(minHeap.peek()<num)
+            {
+                minHeap.poll();
+                minHeap.offer(num);
+            }
+        }
+
+        return minHeap.peek();
+
+    }
+}
