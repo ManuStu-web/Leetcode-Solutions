5ce3cf5be3a64eaff2fbe4cbbd4d07628fc9fa67
Manu Sharma <Msharma.stu@gmail.com>
2026-08-02 21:03:36 +0530

    Leetcode Problem 70

---
+++ DynamicProgramming/ClimbingStairs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int climbStairs(int n) {
+        int prev=1; //imp otherwise wrong result
+        int prev2=1;
+
+        for(int i=2;i<=n;i++)
+        {
+            int curr = prev+prev2;
+            prev2=prev;
+            prev=curr;
+        }
+
+        return prev;
+    }
+}
