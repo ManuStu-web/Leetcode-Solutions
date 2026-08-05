79c691396250de2ac908ed6ba5db173bdc0e4083
Manu Sharma <Msharma.stu@gmail.com>
2026-08-05 21:07:58 +0530

    Leetcode Problem 367

---
+++ Arrays/ValidPerfectSquare.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long lb=1;
+        long ub=num;
+
+        while(lb<=ub)
+        {
+            long mid = lb+(ub-lb)/2;
+            if(mid*mid==num)
+            {
+                return true;
+            }
+            else if(mid*mid>num)
+            {
+                ub=mid-1;
+            }
+            else{
+                lb=mid+1;
+            }
+        }
+
+        return false;
+    }
+}
